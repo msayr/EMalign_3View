64bd49830d937ede4b0b008a4403b2dde99cb91f
ValGillet <val.gillet12@gmail.com>
2025-10-04 11:14:26 +0200

    double to single quote

--- io/progress.py
+++ io/progress.py
@@ -3,11 +3,11 @@ from pymongo import MongoClient
 import json
 
 def get_mongo_client(config_file=None):
-    """
+    '''
     Returns a MongoClient object.
     If config_file is provided, it will be used to configure the client.
     Otherwise, it will use default parameters.
-    """
+    '''
     if config_file:
         with open(config_file, 'r') as f:
             config = json.load(f)
@@ -16,16 +16,16 @@ def get_mongo_client(config_file=None):
         return MongoClient()
 
 def get_mongo_db(client, project_name):
-    """
+    '''
     Returns a database object.
-    """
-    db_name = f"alignment_{project_name}"
+    '''
+    db_name = f'alignment_{project_name}'
     return client[db_name]
 
 def log_progress(db, stack_name, step_name, global_slice_index, local_slice_index, metadata):
-    """
+    '''
     Logs progress to the database.
-    """
+    '''
     collection = db[stack_name]
     doc = {
         'step_name': step_name,
@@ -37,14 +37,17 @@ def log_progress(db, stack_name, step_name, global_slice_index, local_slice_inde
     collection.insert_one(doc)
 
 def check_progress(db, stack_name, step_name, global_slice_index):
-    """
+    '''
     Checks if a slice has already been processed.
-    """
+    '''
     collection = db[stack_name]
     return collection.count_documents({'step_name': step_name, 'global_slice': global_slice_index}) > 0
 
-def wipe_progress(db, stack_name):
-    """
+def wipe_progress(db, stack_name, step_name=None):
+    '''
     Wipes the progress for a given stack.
-    """
-    db.drop_collection(stack_name)
+    '''
+    if step_name is None:
+        db.drop_collection(stack_name)
+    else:
+        db[stack_name].delete_many({'step_name': step_name})

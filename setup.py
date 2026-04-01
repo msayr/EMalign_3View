700d0b6fd73c7cbd873ffeb995d80164e2b9a0ed
Marcel Sayre <52462209+msayr@users.noreply.github.com>
2026-04-01 20:59:52 +1100

    Fix editable install requirement specifiers

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ setup(
             'scipy',
             'pymongo',
             'tqdm',
-            'git+https://github.com/google-research/sofima.git@5c5642cded4b33ec7e975afc3d85f15c0126fb2e'
+            'sofima @ git+https://github.com/google-research/sofima.git@5c5642cded4b33ec7e975afc3d85f15c0126fb2e'
         ],
         extras_require={
-            'neuroglancer'
+            'neuroglancer': ['neuroglancer']
         },
         keywords=['python', 'alignment']
     )
